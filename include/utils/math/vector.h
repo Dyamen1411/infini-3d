f86a3b001c386088f6ecb4213be7c50a9e82a5c0
Lailouezzz <alanlebouder@gmail.com>
2023-10-31 03:20:15 +0100

    Added: Vector function prototypes

--- include/utils/math/vector.h
+++ include/utils/math/vector.h
@@ -6,7 +6,7 @@
 /*   By: ale-boud <ale-boud@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 19:32:56 by ale-boud          #+#    #+#             */
-/*   Updated: 2023/10/31 02:18:36 by ale-boud         ###   ########.fr       */
+/*   Updated: 2023/10/31 03:19:02 by ale-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 // ************************************************************************** //
 
 /**
- * @brief Coordination type
+ * @brief Coordination type.
  */
 typedef float t_coord;
 
@@ -49,5 +49,43 @@ typedef struct s_ovector2d {
 	t_vector2d	pos;
 	t_vector2d	dir;
 }	t_ovector2d;
+
+// ************************************************************************** //
+// *                                                                        * //
+// * Prototypes                                                             * //
+// *                                                                        * //
+// ************************************************************************** //
+
+/**
+ * @brief Vector addition (a.x+b.x, a.y+b.y).
+ * @param a Vector a.
+ * @param b Vector b.
+ * @return #s_vector2d result of the vector addition.
+ */
+t_vector2d	vec2_add(const t_vector2d *a, const t_vector2d *b);
+
+/**
+ * @brief Vector substraction (a.x-b.x, a.y-b.y).
+ * @param a Vector a.
+ * @param b Vector b.
+ * @return #s_vector2d result of the vector substraction.
+ */
+t_vector2d	vec2_sub(const t_vector2d *a, const t_vector2d *b);
+
+/**
+ * @brief Vector dot product (a.x*b.x+a.y*b.y).
+ * @param a Vector a.
+ * @param b Vector b.
+ * @return #t_coord result of the dot product.
+ */
+t_coord		vec2_dot(const t_vector2d *a, const t_vector2d *b);
+
+/**
+ * @brief Vector scalar multiplication (a.x*f,a.y*f).
+ * @param a Vector a.
+ * @param f	Factor f.
+ * @return #s_vector2d result of the vector multiplication.
+ */
+t_vector2d	vec2_mul(const t_vector2d *a, t_coord f);
 
 #endif
